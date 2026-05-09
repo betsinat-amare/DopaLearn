228ac8933efd354d24b4f634244651d0651d2797
Fenet_Ab <abilufenet@gmail.com>
2026-05-09 10:20:22 +0300

    auth module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthorizationModule } from './modules/authorization/authorization.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthorizationModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
